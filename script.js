d35d22082c37dcdc0ce9c30d6669adf1c5a82450
maxPallu <32013577+maxPallu@users.noreply.github.com>
2026-09-17 00:58:30 +0200

    Creates the grid

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 for(let i = 0; i < 256; i++) {
     const container = document.querySelector(".container");
     const div = document.createElement("div");
-    div.classList.add("div");
-    div.textContent = "TEST";
+    div.classList.add("divSquare");
 
     container.appendChild(div);
 }
